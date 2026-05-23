374821b786981a131361a81cfe06ca4e062fa1d9
markno5studio-max <markno.5.studio@gmail.com>
2026-05-23 18:18:22 +0800

    chore: auto-sync content 2026-05-23T10:18:20.706Z

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 /* ================================================================
    MK5 - Published Content (content.js)
-   Published: 2026/5/23 下午6:14:59
+   Published: 2026/5/23 下午6:18:20
    Auto-generated. Do not edit manually.
 ================================================================ */
 
@@ -239,7 +239,7 @@ window.MK5_PUBLISHED = {
   },
   "gmailUrl": "https://mail.google.com/mail/u/2/#inbox",
   "mailToAddress": "",
-  "_publishedAt": "2026-05-23T10:14:59.255Z",
+  "_publishedAt": "2026-05-23T10:18:20.697Z",
   "_cloudOwner": "markno5studio-max",
   "_cloudRepo": "markno-5-studio.github.io",
   "_cloudBranch": "main"
