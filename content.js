4395a2e85c8aacbaa30dfb5ccffa7b2fd06ec20d
markno5studio-max <markno.5.studio@gmail.com>
2026-05-24 16:29:36 +0800

    chore: auto-sync content 2026-05-24T08:29:35.062Z

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 /* ================================================================
    MK5 - Published Content (content.js)
-   Published: 2026/5/24 下午4:28:05
+   Published: 2026/5/24 下午4:29:35
    Auto-generated. Do not edit manually.
 ================================================================ */
 
@@ -301,6 +301,18 @@ window.MK5_PUBLISHED = {
       "title": "【中醫診所】濕疹篇",
       "cat": "企業形象廣告",
       "description": ""
+    },
+    {
+      "url": "https://youtu.be/u6f28feU10w",
+      "title": "2022 東興圳光藝節 TVCF",
+      "cat": "活動宣傳",
+      "description": ""
+    },
+    {
+      "url": "https://youtu.be/XDHZlEbl1PE",
+      "title": "淡水風雲音樂MV",
+      "cat": "音樂錄影帶",
+      "description": ""
     }
   ],
   "theme": {
@@ -349,7 +361,7 @@ window.MK5_PUBLISHED = {
   },
   "gmailUrl": "https://mail.google.com/mail/u/2/#inbox",
   "mailToAddress": "",
-  "_publishedAt": "2026-05-24T08:28:05.351Z",
+  "_publishedAt": "2026-05-24T08:29:35.055Z",
   "_cloudOwner": "markno5studio-max",
   "_cloudRepo": "markno-5-studio.github.io",
   "_cloudBranch": "main"
